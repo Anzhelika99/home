04f57f476769b40533e69ca5be2c40ff049d98e5
Anzhelika <92148852+Anzhelika99@users.noreply.github.com>
2023-10-13 23:16:00 +0700

    изменила изображения, сделала кнопку галереи
    Не готов слайдер галереи, но есть все изоюражения

---
+++ js/galerySlider.js
@@ -0,0 +1,23 @@
+const galerySlider = () => {
+  const swiper = new Swiper(".swiper__galery", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+      nextEl: ".next-button",
+      prevEl: ".prev-button",
+    },
+    breakpoints: {
+      940: {
+        slidesPerView: 2,
+        spaceBetween: 44,
+      },
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 44,
+      },
+    },
+  });
+
+  document.getElementById("popup-galery").style.display = "block";
+}
+galerySlider()
