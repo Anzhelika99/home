fd2c956b6164b180848a54011a5b9194e775b926
Anzhelika <92148852+Anzhelika99@users.noreply.github.com>
2023-10-21 15:35:03 +0700

    Add files via upload

--- send.php
+++ send.php
@@ -3,32 +3,28 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require "PHPMailer/src/Exception.php";
-    require "PHPMailer/src/PHPMailer.php";
+    require "phpmailer/src/Exception.php";
+    require "phpmailer/src/PHPMailer.php";
 
-    $mail = new PHPMailer(true); /* Создаем объект MAIL */
-    $mail->CharSet = "UTF-8"; /* Задаем кодировку UTF-8 */
+    $mail = new PHPMailer(true);
+    $mail->CharSet = "UTF-8"; 
     $mail->setLanguage('ru', 'phpmailer/language/')
-    $mail->IsHTML(true); /* Разрешаем работу с HTML */
+    $mail->IsHTML(true); 
 
     $mail->setFrom('a.nitser@yandex.ru', 'Анжелика Ницер'); //От кого
     $mail->addAddress("anzhelika.nitser@yandex.ru"); /* Здесь введите Email, куда отправлять */
     $mail->Subject = "Заявка с формы сайта Стильный Дом"; /* Тема письма */
 
-    $email = $_POST["email"]; /* Почту */
-    $phone = $_POST["phone"]; /* Телефон */
-    $message = $_POST["message"]; /* Сообщение с формы */
-
     $body = '<h1>Заявка с формы сайта Стильный Дом</h1>'
     
     if (trim(!empty($email))) {
-      $body.= '<p><strong>Email:</strong>' .$email'</p>'
+      $body.= '<p><strong>Email:</strong>' .$_POST["email"]'</p>'
     }
     if(trim(!empty($phone))) {
-      $body.= '<p><strong>Телефон:</strong>' .$phone'</p>'
+      $body.= '<p><strong>Телефон:</strong>' .$_POST["phone"]'</p>'
     }
     if(trim(!empty($message))) {
-      $body.= '<p><strong>Сообщение:</strong>' .$message'</p>'
+      $body.= '<p><strong>Сообщение:</strong>' .$_POST["message"]'</p>'
     }
 
 
